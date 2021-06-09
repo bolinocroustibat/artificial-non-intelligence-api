a4b2aced7c8c9f7b4f91782d72cfda2dc03e2ad0
Adrien Carpentier <moi@adriencarpentier.com>
2021-06-09 21:05:01 +0900

    Restrict to only app origin for production usage

--- api/main.py
+++ api/main.py
@@ -14,12 +14,12 @@ app = FastAPI()
 # For security (avoid another app to connect to this API)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins (debug, remove for production)
-    # allow_origins=[
-    #     "http://localhost",
-    #     "http://127.0.0.1",
-    #     "https://artificial-non-intelligence.herokuapp.com"
-    # ],  # Allows only the frontend origin, use that for production
+    # allow_origins=["*"],  # Allows all origins (debug, remove for production)
+    allow_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
+        "https://artificial-non-intelligence.herokuapp.com"
+    ],  # Allows only the frontend origin, use that for production
     allow_credentials=True,
     # allow_methods=["*"],  # Allows all methods
     allow_methods=["GET"],  # Allows only GET method
